11f57bc5ba158e00412fb3f9db05eafa0d8977bc
whynrmd <wahyuniramadhani1011@gmail.com>
2023-12-09 13:15:26 +0700

    Jobsheet5

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        //jangan lupa import model Post dengan cara klik kanan untuk import
+        $post = Post::all();
+        //berfungsi untuk mengatur jenis data yang akan ditampilkan
+        return response()->json(['data' => $post]);
+    }
+
+}
+
